4e729938efba1ce330c0df1fd28628bcc53d55b9
rishabh1230 <rishabhkis0710@gmail.com>
2025-05-27 23:21:46 +0530

    User and video model and bcrypt jwt etc

---
+++ src/utils/asyncHandeler.js
@@ -0,0 +1,29 @@
+const asyncHandeler = (requestHandeler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandeler(req, res, next)).
+        catch((err) => next(err))
+    }
+}
+
+
+
+
+
+export {asyncHandeler}
+
+
+// //const asyncHandeler = () => {}
+// //const asyncHandeler = (func) => () => {}
+// //const asyncHandeler = (func) => async () => {}
+
+
+// const asyncHandeler =(fn) => async (req, res, next) => {
+//     try{
+//         await fn(req, res, next)
+//     }catch (error){
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
